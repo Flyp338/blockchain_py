22bd87089659cd6bec9d3a14dd40a036b88cbb73
Flyp338 <presta.alessandro338@gmail.com>
2026-09-16 22:51:04 +0200

    implemented register POST method and consensus GET method

--- app.py
+++ app.py
@@ -15,34 +15,60 @@ node_id = str(uuid4()).replace("-", "")
 blockchain = BlockChain()
 
 
-@app.route("/mine", methods=['GET'])
+@app.route("/mine", methods=["GET"])
 def mine():
     # we run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
-    last_proof = last_block['proof']
+    last_proof = last_block["proof"]
     proof = blockchain.PoW(last_proof)
 
     # we must receive a reward for finding the proof
     # the sender is '0' to signify that this node has mined a new coin.
-    blockchain.new_transaction(
-        sender = '0',
-        recipient= node_id,
-        amount= 1
-    )
+    blockchain.new_transaction(sender="0", recipient=node_id, amount=1)
 
     # forge the new block by adding it to the chain
     previous_hash = blockchain.hash(last_block)
     block = blockchain.new_block(proof, previous_hash)
 
     response = {
-        'message' : 'New Block Forged',
-        'index' : block['index'],
-        'transactions' : block['transactions'],
-        'proof' : block['proof'],
-        'previous_hash' : block['previous_hash']
+        "message": "New Block Forged",
+        "index": block["index"],
+        "transactions": block["transactions"],
+        "proof": block["proof"],
+        "previous_hash": block["previous_hash"],
     }
 
-    return jsonify(response), 200 
+    return jsonify(response), 200
+
+
+@app.route("/nodes/register", methods=["POST"])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get("nodes")
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        "message": "New nodes have been added",
+        "total_nodes": list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route("/nodes/resolve", methods=["GET"])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {"message": "Our chain was replaced", "new_chain": blockchain.chain}
+    else:
+        response = {"message": "Our chain is authoritative", "chain": blockchain.chain}
+
+    return jsonify(response), 200
 
 
 @app.route("/transactions/new", methods=["POST"])
